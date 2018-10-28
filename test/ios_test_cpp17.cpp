dc3e9e3a0ae1947fdb71efe68e368c216d6fd278
Patrick Wang-Freninger <github@freninger.at>
2018-10-28 18:27:48 +0100

    Test C++17 class template deduction with scope_guard classes.

--- test/ios_test_cpp17.cpp
+++ test/ios_test_cpp17.cpp
@@ -6,5 +6,5 @@
 #include <iostream>
 
 #ifdef __cpp_deduction_guides
-SCENARIO("class template deduction") { ostream_capture c(std::cout); }
+SCENARIO("ios types") { ostream_capture c(std::cout); }
 #endif
---
+++ test/scope_guard_test_cpp17.cpp
@@ -0,0 +1,11 @@
+#include "cpp_utils/scope_guard.hpp"
+
+#include "catch.hpp"
+
+#ifdef __cpp_deduction_guides
+SCENARIO("scope_guard types.")
+{
+    exit_guard      g1([] {});
+    on_return_guard g2([] {});
+}
+#endif
