472fe4d6afcbc6ff1a449dca1cd76e0e1e141d13
Patrick Wang-Freninger <github@freninger.at>
2019-03-16 22:11:45 +0100

    Fix restricting archs and build_types

--- build.py
+++ build.py
@@ -4,6 +4,6 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(archs=["x86_64"], build_types=["Debug"])
+    builder = ConanMultiPackager(archs=["x86_64"], build_types=["Debug"])
+    builder.add_common_builds()
     builder.run()
