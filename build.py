8bd4caaf666cbc85843a30103d6d8c35400aa334
Patrick Wang-Freninger <github@freninger.at>
2019-03-16 21:33:53 +0100

    Go back to ConanMultiPackager

--- build.py
+++ build.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bincrafters import build_template_boost_header_only
-
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = build_template_boost_header_only.get_builder()
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
     builder.run()
-    
