2db375fc11c76e114f9776927f92afe1b3e61d8b
Patrick Wang-Freninger <github@freninger.at>
2019-03-16 22:05:06 +0100

    Only build for one architecture and build_type

--- build.py
+++ build.py
@@ -5,5 +5,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add_common_builds(archs=["x86_64"], build_types=["Debug"])
     builder.run()
