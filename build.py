f9d03b104af6ec47ef971ad62de8a2db3df531d0
Patrick Wang-Freninger <github@freninger.at>
2019-03-17 17:15:48 +0100

    Build with both libstdc++ and libstdc++11

--- build.py
+++ build.py
@@ -4,7 +4,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"], build_types=["Debug"])
+    builder = ConanMultiPackager(pure_c=False, archs=["x86_64"], build_types=["Debug"])
     builder.add_common_builds()
     builder.update_build_if(lambda build: build.settings["compiler"] == "clang" and build.settings["compiler.version"] <= "4.0",
                             new_settings={"compiler.libcxx": "libc++"})
